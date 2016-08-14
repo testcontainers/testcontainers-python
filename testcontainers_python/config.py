e9e26b6d45362c5f9064c8d4cf788b92b96ebc9d
sergey <sergey@gmail.com>
2016-08-14 20:04:59 +0300

    config changed. containers changed to debug versions

--- testcontainers_python/config.py
+++ testcontainers_python/config.py
@@ -2,5 +2,6 @@ docker_base_url = 'unix://var/run/docker.sock'
 max_tries = 60
 sleep_time = 1
 hub = {'image': 'selenium/hub:2.53.0', 'bind_ports': {4444: 4444}, 'name': 'selenium-hub'}
-ff_node = {'image': 'selenium/node-firefox:2.53.0', 'links': {'selenium-hub': 'hub'}}
-chrome_node = {'image': 'selenium/node-chrome:2.53.0', 'links': {'selenium-hub': 'hub'}}
+ff_node = {'image': 'selenium/node-firefox-debug:2.53.0', 'links': {'selenium-hub': 'hub'}, 'bind_ports': {5900: 5900}}
+chrome_node = {'image': 'selenium/node-chrome-debug:2.53.0', 'links': {'selenium-hub': 'hub'},
+               'bind_ports': {5900: 5901}}
