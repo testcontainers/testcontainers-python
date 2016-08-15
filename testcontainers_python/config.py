507343154676692d54651119a0c5e46aec0819fd
sepi <sepi@ciklum.com>
2016-08-15 12:56:56 +0300

    added workaround for mac os

--- testcontainers_python/config.py
+++ testcontainers_python/config.py
@@ -1,7 +1,23 @@
 docker_base_url = 'unix://var/run/docker.sock'
 max_tries = 60
 sleep_time = 1
-hub = {'image': 'selenium/hub:2.53.0', 'bind_ports': {4444: 4444}, 'name': 'selenium-hub'}
-ff_node = {'image': 'selenium/node-firefox-debug:2.53.0', 'links': {'selenium-hub': 'hub'}, 'bind_ports': {5900: 5900}}
-chrome_node = {'image': 'selenium/node-chrome-debug:2.53.0', 'links': {'selenium-hub': 'hub'},
-               'bind_ports': {5900: 5901}}
+
+hub = {
+    'image': 'selenium/hub:2.53.0',
+    'bind_ports': {4444: 4444},
+    'name': 'selenium-hub'
+}
+
+firefox_node = {
+    'image': 'selenium/node-firefox-debug:2.53.0',
+    'links': {'selenium-hub': 'hub'},
+    'bind_ports': {5900: 5900},
+    'env': ['no_proxy=localhost', 'HUB_ENV_no_proxy=localhost']
+}
+
+chrome_node = {
+    'image': 'selenium/node-chrome-debug:2.53.0',
+    'links': {'selenium-hub': 'hub'},
+    'bind_ports': {5900: 5901},
+    'env': ['no_proxy=localhost', 'HUB_ENV_no_proxy=localhost']
+}
