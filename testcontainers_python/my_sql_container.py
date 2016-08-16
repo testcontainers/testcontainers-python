6b6efbc84c2c90c287b4c44b2a984b925fbbc8b3
sepi <sepi@ciklum.com>
2016-08-16 16:26:13 +0300

    MySql container added

--- testcontainers_python/my_sql_container.py
+++ testcontainers_python/my_sql_container.py
@@ -1,5 +1,11 @@
+from time import sleep
+
 from testcontainers_python import config
+from testcontainers_python.brogress_bar import ConsoleProgressBar
 from testcontainers_python.docker_client import DockerClient
+import MySQLdb
+
+from testcontainers_python.exceptions import TimeoutException
 
 
 class MySqlContainer(object):
@@ -7,6 +13,7 @@ class MySqlContainer(object):
         self._docker = DockerClient()
         self.image = image
         self._containers = []
+        self._connection = None
 
     def __enter__(self):
         return self.start()
@@ -22,11 +29,30 @@ class MySqlContainer(object):
         mysql = self._docker.run(self.image, bind_ports={3306: 3306},
                                  env={"MYSQL_ROOT_PASSWORD": config.my_sql_root_password})
         self._containers.append(mysql)
+        self._connection = self._wait_for_container_to_start(mysql)
+        return self
+
+    def _wait_for_container_to_start(self, container):
+        hub_info = self._docker.port(container, 3306)[0]
+        bar = ConsoleProgressBar().bar
+        for _ in bar(range(0, config.max_tries)):
+            try:
+                return MySQLdb.connect(host=hub_info['HostIp'],  # your host, usually localhost
+                                       user="root",  # your username
+                                       passwd=config.my_sql_root_password,  # your password
+                                       db="test")
+            except Exception:
+                sleep(config.sleep_time)
+        raise TimeoutException("Wait time exceeded {} sec.".format(config.max_tries))
 
     def stop(self):
         """
         Stop all spawned containers
         :return:
         """
+        self._connection.close()
         for cont in self._containers:
             self._docker.remove(cont, True)
+
+    def get_connection(self):
+        return self._connection
