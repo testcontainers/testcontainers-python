52735e4a625ce0996496f900a97855cf1d76a2d5
sepi <sepi@ciklum.com>
2016-08-17 12:48:36 +0300

    setup py changed

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     description='Library provides lightweight, throwaway instances of common databases, Selenium web browsers, or anything else that can run in a Docker containe',
     author='Sergey Pirogov',
     author_email='automationremarks@gmail.com',
-    url='https://github.com/SergeyPirogov/video_recorder_python',  # use the URL to the github repo
-    download_url='https://github.com/SergeyPirogov/video_recorder_python/tarball/1.0',  # I'll explain this in a second
+    url='https://github.com/SergeyPirogov/testcontainers_python',  # use the URL to the github repo
+    download_url='https://github.com/SergeyPirogov/testcontainers_python/tarball/1.0',  # I'll explain this in a second
     keywords=['testing', 'logging', 'docker', 'test automation'],  # arbitrary keywords
     classifiers=[],
 )
