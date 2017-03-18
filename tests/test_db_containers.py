37c9d139daa1014e403a3b66cbbf28e389edb461
sergey <sergey@gmail.com>
2017-03-18 23:38:40 +0200

    travis integration

--- tests/test_db_containers.py
+++ tests/test_db_containers.py
@@ -1,6 +1,9 @@
 import psycopg2
 import sqlalchemy
+from pymongo import MongoClient
 
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.waiting_utils import wait_container_is_ready
 from testcontainers.mysql import MySqlContainer, MariaDbContainer
 from testcontainers.postgres import PostgresContainer
 
@@ -35,33 +38,32 @@ def test_docker_run_mariadb():
         for row in result:
             assert row[0] == '10.1.17-MariaDB-1~jessie'
 
-# def test_docker_generic_db():
-#     mongo_container = DockerContainer(image_name="mongo",
-#                                       version="latest",
-#                                       container_name="mongodb")
-#     mongo_container.bind_ports(27017, 27017)
-#
-#     with mongo_container:
-#         @wait_container_is_ready()
-#         def connect():
-#             return MongoClient("mongodb://0.0.0.0:27017")
-#
-#         db = connect().primer
-#         result = db.restaurants.insert_one(
-#             {
-#                 "address": {
-#                     "street": "2 Avenue",
-#                     "zipcode": "10075",
-#                     "building": "1480",
-#                     "coord": [-73.9557413, 40.7720266]
-#                 },
-#                 "borough": "Manhattan",
-#                 "cuisine": "Italian",
-#                 "name": "Vella",
-#                 "restaurant_id": "41704620"
-#             }
-#         )
-#         print(result.inserted_id)
-#         cursor = db.restaurants.find({"borough": "Manhattan"})
-#         for document in cursor:
-#             print(document)
+
+def test_docker_generic_db():
+    mongo_container = DockerContainer("mongo", "latest")
+    mongo_container.expose_port(27017, 27017)
+
+    with mongo_container:
+        @wait_container_is_ready()
+        def connect():
+            return MongoClient("mongodb://0.0.0.0:27017")
+
+        db = connect().primer
+        result = db.restaurants.insert_one(
+            {
+                "address": {
+                    "street": "2 Avenue",
+                    "zipcode": "10075",
+                    "building": "1480",
+                    "coord": [-73.9557413, 40.7720266]
+                },
+                "borough": "Manhattan",
+                "cuisine": "Italian",
+                "name": "Vella",
+                "restaurant_id": "41704620"
+            }
+        )
+        print(result.inserted_id)
+        cursor = db.restaurants.find({"borough": "Manhattan"})
+        for document in cursor:
+            print(document)
--- tests/test_docker_container.py
+++
@@ -1,8 +0,0 @@
-from testcontainers.core.generic import DockerContainer
-
-
-def test_container_port():
-    cont = DockerContainer("selenium/hub", "latest", "selenium_hub")
-    cont.start()
-
-    assert cont.get_host_info(4444) > 0
