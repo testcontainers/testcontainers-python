904c5bcd981f7346b4235d829bf759d99c130cd7
sergey <sergey@gmail.com>
2017-03-20 20:09:43 +0200

    config clean up pep8

--- testcontainers/core/config.py
+++ testcontainers/core/config.py
@@ -1,11 +1,4 @@
-import os
+from os import environ
 
-import yaml
-
-
-def get_env(env, default):
-    return os.environ.get(env, default)
-
-
-MAX_TRIES = int(get_env("TC_MAX_TRIES", 120))
-SLEEP_TIME = int(get_env("TC_POOLING_INTERVAL", 1))
+MAX_TRIES = int(environ.get("TC_MAX_TRIES", 120))
+SLEEP_TIME = int(environ.get("TC_POOLING_INTERVAL", 1))
