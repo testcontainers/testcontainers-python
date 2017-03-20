2060d641e516315fdb8ec2f154fdb17f2f4253b0
sergey <sergey@gmail.com>
2017-03-20 20:08:01 +0200

    travis yml chrome added

--- testcontainers/core/config.py
+++ testcontainers/core/config.py
@@ -9,12 +9,3 @@ def get_env(env, default):
 
 MAX_TRIES = int(get_env("TC_MAX_TRIES", 120))
 SLEEP_TIME = int(get_env("TC_POOLING_INTERVAL", 1))
-
-
-class Configuration(object):
-    def __init__(self, filename="config.yml"):
-        with open(filename, 'r') as ymlfile:
-            self.cfg = yaml.load(ymlfile)
-
-    def get(self, section):
-        return self.cfg[section]
--- tests/test_config.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-
-
-d = cfg['mysql']
-
-for k, v in d.items():
-    print(k, v)
