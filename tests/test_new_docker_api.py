b40eb9414d5b4208c831a82dcd44e46ee62034fb
sergey <sergey@gmail.com>
2017-03-22 21:07:19 +0200

    test for env variables fixed

--- tests/test_new_docker_api.py
+++ tests/test_new_docker_api.py
@@ -3,12 +3,15 @@ import os
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
+from testcontainers import mysql
+
 from testcontainers.core.generic import GenericContainer
 
-os.environ["MYSQL_USER"] = "demo"
-os.environ["MYSQL_DATABASE"] = "custom_db"
+from importlib import reload
 
-from testcontainers.mysql import MySqlContainer
+def setup_module(m):
+    os.environ["MYSQL_USER"] = "demo"
+    os.environ["MYSQL_DATABASE"] = "custom_db"
 
 
 def test_docker_custom_image():
@@ -22,9 +25,11 @@ def test_docker_custom_image():
 
 
 def test_docker_env_variables():
-    mysql = MySqlContainer()
-    mysql.bind_ports(3306, 32785)
-    print(mysql.MYSQL_USER, mysql.MYSQL_DATABASE)
-    with mysql:
-        url = mysql.get_connection_url()
+    reload(mysql)
+
+    db = mysql.MySqlContainer()
+    db.bind_ports(3306, 32785)
+    print(db.MYSQL_USER, db.MYSQL_DATABASE)
+    with db:
+        url = db.get_connection_url()
         assert url == 'mysql+pymysql://demo:test@0.0.0.0:32785/custom_db'
