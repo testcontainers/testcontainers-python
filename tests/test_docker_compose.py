0294d0c791bc9bd2f8e94fd195c33f9acabae5fc
Sergey Pirogov <sergio_89@ukr.net>
2017-05-07 10:16:57 +0300

    docker-compose  added

---
+++ tests/test_docker_compose.py
@@ -0,0 +1,29 @@
+import os
+
+import pytest
+
+from testcontainers.compose import DockerCompose
+from testcontainers.core.exceptions import NoSuchPortExposed
+
+
+def test_can_spawn_service_via_compose():
+    compose = DockerCompose(os.path.dirname(__file__))
+
+    try:
+        compose.start()
+        host = compose.get_service_host("hub", 4444)
+        port = compose.get_service_port("hub", 4444)
+        assert host == "0.0.0.0"
+        assert port == "4444"
+    finally:
+        compose.stop()
+
+
+def test_can_throw_exception_if_no_port_exposed():
+    compose = DockerCompose(os.path.dirname(__file__))
+
+    compose.start()
+    with pytest.raises(NoSuchPortExposed):
+        compose.get_service_host("hub", 5555)
+
+    compose.stop()
