5ec936d3d9c36b5990d1958f2119cbf93e4507d7
t0ch1k <tochilkin.alex@gmail.com>
2017-12-20 18:43:34 +0200

    version changed

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ import setuptools
 setuptools.setup(
     name='testcontainers',
     packages=setuptools.find_packages(exclude=['tests']),
-    version='2.1.0',
+    version='2.1.1',
     description=('Library provides lightweight, throwaway '
                  'instances of common databases, '
                  'Selenium web browsers, or anything else that can '
