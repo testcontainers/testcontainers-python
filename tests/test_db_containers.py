46acd11cc79f971cf2643e36436d1df13a6c16ff
Danilo <danilo.abrignani@leitha.eu>
2020-01-07 10:42:14 +0100

    remove comments from test

--- tests/test_db_containers.py
+++ tests/test_db_containers.py
@@ -37,18 +37,18 @@ def test_docker_run_mariadb():
             assert row[0] == '10.2.9-MariaDB-10.2.9+maria~jessie'
 
 
-# @pytest.mark.skip(reason="needs oracle client libraries unavailable on Travis")
-# def test_docker_run_oracle():
-#     oracledb_container = OracleDbContainer()
-#     with oracledb_container as oracledb:
-#         e = sqlalchemy.create_engine(oracledb.get_connection_url())
-#         result = e.execute("select * from V$VERSION")
-#         versions = {'Oracle Database 11g Express Edition Release 11.2.0.2.0 - 64bit Production',
-#                     'PL/SQL Release 11.2.0.2.0 - Production',
-#                     'CORE\t11.2.0.2.0\tProduction',
-#                     'TNS for Linux: Version 11.2.0.2.0 - Production',
-#                     'NLSRTL Version 11.2.0.2.0 - Production'}
-#         assert {row[0] for row in result} == versions
+@pytest.mark.skip(reason="needs oracle client libraries unavailable on Travis")
+def test_docker_run_oracle():
+    oracledb_container = OracleDbContainer()
+    with oracledb_container as oracledb:
+        e = sqlalchemy.create_engine(oracledb.get_connection_url())
+        result = e.execute("select * from V$VERSION")
+        versions = {'Oracle Database 11g Express Edition Release 11.2.0.2.0 - 64bit Production',
+                    'PL/SQL Release 11.2.0.2.0 - Production',
+                    'CORE\t11.2.0.2.0\tProduction',
+                    'TNS for Linux: Version 11.2.0.2.0 - Production',
+                    'NLSRTL Version 11.2.0.2.0 - Production'}
+        assert {row[0] for row in result} == versions
 
 
 def test_docker_run_mongodb():
