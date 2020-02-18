9269d1aec45a9b49446a0babfaff2f45a6a3828f
Till Hoffmann <till@spotify.com>
2020-02-18 18:55:52 +0000

    Enable flake8.

--- testcontainers/general.py
+++ testcontainers/general.py
@@ -11,12 +11,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 from testcontainers.core.container import DockerContainer
-from testcontainers.core.waiting_utils import wait_container_is_ready
 
 
 class TestContainer(DockerContainer):
     def __init__(self, image, port_to_expose=None):
-        super(RedisContainer, self).__init__(image)
+        super(TestContainer, self).__init__(image)
         if port_to_expose:
-		self.port_to_expose = port_to_expose
-	        self.with_exposed_ports(self.port_to_expose)
+            self.port_to_expose = port_to_expose
+            self.with_exposed_ports(self.port_to_expose)
