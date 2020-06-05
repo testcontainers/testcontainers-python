bb268d436d8052921c17c3d90339661130f7cb8c
kezhenxu94 <kezhenxu94@163.com>
2020-06-05 23:46:44 +0800

    Support specifying multiple docker-compose.yml files

--- testcontainers/compose.py
+++ testcontainers/compose.py
@@ -57,7 +57,7 @@ class DockerCompose(object):
             compose_file_name="docker-compose.yml",
             pull=False):
         self.filepath = filepath
-        self.compose_file_name = compose_file_name
+        self.compose_file_names = compose_file_name if type(compose_file_name) is list else [compose_file_name]
         self.pull = pull
 
     def __enter__(self):
@@ -67,18 +67,24 @@ class DockerCompose(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.stop()
 
+    def docker_compose_command(self):
+        docker_compose_cmd = ['docker-compose']
+        for file in self.compose_file_names:
+            docker_compose_cmd += ['-f', file]
+        return docker_compose_cmd
+
     def start(self):
         with blindspin.spinner():
             if self.pull:
-                subprocess.call(["docker-compose", "-f", self.compose_file_name, "pull"],
-                                cwd=self.filepath)
-            subprocess.call(["docker-compose", "-f", self.compose_file_name, "up", "-d"],
-                            cwd=self.filepath)
+                pull_cmd = self.docker_compose_command() + ['pull']
+                subprocess.call(pull_cmd, cwd=self.filepath)
+            up_cmd = self.docker_compose_command() + ['up']
+            subprocess.call(up_cmd, cwd=self.filepath)
 
     def stop(self):
         with blindspin.spinner():
-            subprocess.call(["docker-compose", "-f", self.compose_file_name, "down", "-v"],
-                            cwd=self.filepath)
+            down_cmd = self.docker_compose_command() + ['down', '-v']
+            subprocess.call(down_cmd, cwd=self.filepath)
 
     def get_service_port(self, service_name, port):
         return self._get_service_info(service_name, port)[1]
@@ -87,9 +93,8 @@ class DockerCompose(object):
         return self._get_service_info(service_name, port)[0]
 
     def _get_service_info(self, service, port):
-        cmd_as_list = ["docker-compose", "-f", self.compose_file_name, "port", service, str(port)]
-        output = subprocess.check_output(cmd_as_list,
-                                         cwd=self.filepath).decode("utf-8")
+        port_cmd = self.docker_compose_command() + ["port", service, str(port)]
+        output = subprocess.check_output(port_cmd, cwd=self.filepath).decode("utf-8")
         result = str(output).rstrip().split(":")
         if len(result) == 1:
             raise NoSuchPortExposed("Port {} was not exposed for service {}"
