a9040aa871edf37f0ea435a59b392d79ec80fb41
kezhenxu94 <kezhenxu94@163.com>
2020-06-12 21:46:07 +0800

    Fix typo

--- tests/test_docker_compose.py
+++ tests/test_docker_compose.py
@@ -44,4 +44,4 @@ def test_can_parse_multiple_compose_files():
         host = compose.get_service_host("hub", 4444)
         port = compose.get_service_port("hub", 4444)
         assert host == "0.0.0.0"
-        assert port == "3306"
+        assert port == "4444"
