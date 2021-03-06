613fcebba9fd2d862a06996f0071402d0e338659
Till Hoffmann <tillahoffmann@gmail.com>
2021-03-06 14:03:33 +0100

    Support old syntax in generate_version.py.

--- generate_version.py
+++ generate_version.py
@@ -7,6 +7,6 @@ if github_ref.startswith(prefix):
     version = github_ref[len(prefix):]
     with open('VERSION', 'w') as fp:
         fp.write(version)
-    print(f'Wrote version {version} to VERSION file.')
+    print('Wrote version %s to VERSION file.' % version)
 else:
-    print(f'Could not identify version in {github_ref}.')
+    raise ValueError('Could not identify version in %s.' % github_ref)
