95ac492c2271203f1f002b19ea199e23c0ef724b
Till Hoffmann <tillahoffmann@gmail.com>
2021-03-06 14:49:03 +0100

    Drop support for retired 3.5 and update dependencies. (#132)

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setuptools.setup(
         'Intended Audience :: Information Technology',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
