3038b52a34377a0632dc43307a0b50715aaf7678
Anton Petrosyuk <antonpetrosyuk@Antons-Air.lan>
2021-04-12 23:42:51 -0400

    fixed flake8

--- tests/test_docker_compose.py
+++ tests/test_docker_compose.py
@@ -57,8 +57,8 @@ def test_can_get_logs():
 
 
 def test_can_pass_env_params_by_env_file():
-    with DockerCompose('tests', compose_file_name='docker-compose-3.yml', env_file='.env.test') as compose:
+    with DockerCompose('tests', compose_file_name='docker-compose-3.yml',
+                       env_file='.env.test') as _:
         check_env_is_set_cmd = 'docker exec tests_mysql_1 printenv | grep TEST_ASSERT_KEY'.split()
         out = subprocess.run(check_env_is_set_cmd, stdout=subprocess.PIPE)
         assert out.stdout.decode('utf-8').splitlines()[0], 'test_is_passed'
-
