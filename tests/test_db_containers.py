9ce79b0308626441f045cbebd21bd30bf9a12a92
Till Hoffmann <tillahoffmann@gmail.com>
2021-12-14 11:34:29 -0500

    Add missing skipif reason.

--- tests/test_db_containers.py
+++ tests/test_db_containers.py
@@ -14,7 +14,8 @@ from testcontainers.oracle import OracleDbContainer
 from testcontainers.postgres import PostgresContainer
 
 
-@pytest.mark.skipif(platform.machine() in ('arm64', 'aarch64'))
+@pytest.mark.skipif(platform.machine() in ('arm64', 'aarch64'),
+                    reason=f'mysql container not available for {platform.machine()}')
 def test_docker_run_mysql():
     config = MySqlContainer('mysql:5.7.17')
     with config as mysql:
