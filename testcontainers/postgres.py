a3b183849a0b7bee9ce00f0adb3ae7c15a73296d
Karolis Labrencis <karolis@labrencis.lt>
2022-04-04 22:38:01 +0300

    Update postgres.py

--- testcontainers/postgres.py
+++ testcontainers/postgres.py
@@ -32,7 +32,12 @@ class PostgresContainer(DbContainer):
     POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "test")
     POSTGRES_DB = os.environ.get("POSTGRES_DB", "test")
 
-    def __init__(self, image="postgres:latest", port=5432, user=None, password=None, dbname=None, driver="psycopg2"):
+    def __init__(self, 
+                 image="postgres:latest", 
+                 port=5432, user=None, 
+                 password=None, 
+                 dbname=None, 
+                 driver="psycopg2"):
         super(PostgresContainer, self).__init__(image=image)
         self.POSTGRES_USER = user or self.POSTGRES_USER
         self.POSTGRES_PASSWORD = password or self.POSTGRES_PASSWORD
