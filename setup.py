5f8705ac1d893863c319eb73e2663cbcde1a6fba
Netanel Shine <nshine@users.noreply.github.com>
2022-06-05 20:39:49 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setuptools.setup(
         'rabbitmq': ['pika'],
         'clickhouse': ['clickhouse-driver'],
         'keycloak': ['python-keycloak'],
-        'arango':['python-arango']
+        'arangodb':['python-arango']
     },
     long_description_content_type="text/x-rst",
     long_description=long_description,
