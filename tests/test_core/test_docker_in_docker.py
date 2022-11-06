ad8cd8e36a1fe51aa95a0651c220d56a0d178b2d
Tim Voets <voets.tim@gmail.com>
2022-11-06 19:13:26 +0100

    refactor, small fixes
    Signed-off-by: Tim Voets <voets.tim@gmail.com>

--- tests/test_core/test_docker_in_docker.py
+++ tests/test_core/test_docker_in_docker.py
@@ -1,24 +1,22 @@
-import pytest
-import pprint
-
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.docker_client import DockerClient
-from testcontainers.core.waiting_utils import wait_for_logs, wait_container_is_ready
+from testcontainers.core.waiting_utils import wait_for_logs
 
 
 def test_wait_for_logs_docker_in_docker():
-    # real dind isn't possible in CI, forwarding the socket to a container port is at least somewhat the same
+    # real dind isn't possible (AFAIK) in CI, forwarding the socket to a container port is at least somewhat the same
     client = DockerClient()
-    dind = client.run(
+    not_really_dind = client.run(
         image="alpine/socat",
         command="tcp-listen:2375,fork,reuseaddr unix-connect:/var/run/docker.sock",
         volumes={'/var/run/docker.sock': {'bind': '/var/run/docker.sock'}},
         detach=True,
     )
 
-    dind.start()
+    not_really_dind.start()
 
-    specs = client.get_container(dind.id)
+    # get ip address for DOCKER_HOST, avoiding DockerContainer class here to prevent code changes affecting the test
+    specs = client.get_container(not_really_dind.id)
     docker_host_ip = specs['NetworkSettings']['Networks']['bridge']['IPAddress']
     docker_host = f"tcp://{docker_host_ip}:2375"
 
@@ -27,7 +25,8 @@ def test_wait_for_logs_docker_in_docker():
             docker_client_kw={
                 "environment": {
                     "DOCKER_HOST": docker_host,
-                    "DOCKER_TLS_CERTDIR": ""
+                    "DOCKER_CERT_PATH": "",
+                    "DOCKER_TLS_VERIFY": ""
                 }
             }) as container:
         assert container.get_container_host_ip() == docker_host_ip
@@ -35,5 +34,5 @@ def test_wait_for_logs_docker_in_docker():
         stdout, stderr = container.get_logs()
         assert stdout, 'There should be something on stdout'
 
-    dind.stop()
-    dind.remove()
+    not_really_dind.stop()
+    not_really_dind.remove()
