01b1ab07597363df5eb94062567798c427c9a684
Till Hoffmann <tillahoffmann@gmail.com>
2023-01-06 14:23:23 -0500

    Move mysql test from core to mysql package.

--- core/tests/test_new_docker_api.py
+++ core/tests/test_new_docker_api.py
@@ -1,8 +1,6 @@
 import os
-import re
 from pathlib import Path
 
-from testcontainers import mysql
 from testcontainers.core.container import DockerContainer
 
 
@@ -19,15 +17,6 @@ def test_docker_custom_image():
     with container:
         port = container.get_exposed_port(3306)
         assert int(port) > 0
-
-
-def test_docker_env_variables():
-    container = mysql.MySqlContainer("mariadb:10.6.5")\
-        .with_bind_ports(3306, 32785).maybe_emulate_amd64()
-    with container:
-        url = container.get_connection_url()
-        pattern = r'mysql\+pymysql:\/\/demo:test@[\w,.]+:(3306|32785)\/custom_db'
-        assert re.match(pattern, url)
 
 
 def test_docker_kwargs():
