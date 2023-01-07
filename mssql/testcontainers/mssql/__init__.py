e072dbacf04b38aa0fe036e1bfa79e697fd9b8da
Till Hoffmann <tillahoffmann@gmail.com>
2023-01-06 19:48:39 -0500

    Remove static variables from `mssql`.

--- mssql/testcontainers/mssql/__init__.py
+++ mssql/testcontainers/mssql/__init__.py
@@ -20,27 +20,30 @@ class SqlServerContainer(DbContainer):
             ...        result = connection.execute(sqlalchemy.text("select @@VERSION"))
     """
 
-    def __init__(self, image: str = "mcr.microsoft.com/mssql/server:2019-latest", user: str = "SA",
-                 password: Optional[str] = None, port: int = 1433, dbname: str = "tempdb",
-                 dialect: str = 'mssql+pymssql', **kwargs) -> None:
+    def __init__(self, image: str = "mcr.microsoft.com/mssql/server:2019-latest",
+                 username: str = "SA", password: Optional[str] = None, port: int = 1433,
+                 dbname: str = "tempdb", dialect: str = 'mssql+pymssql', user: None = None,
+                 **kwargs) -> None:
         super(SqlServerContainer, self).__init__(image, **kwargs)
+        if user:
+            raise ValueError("use `username` instead")
 
         self.port_to_expose = port
         self.with_exposed_ports(self.port_to_expose)
 
-        self.SQLSERVER_PASSWORD = password or environ.get("SQLSERVER_PASSWORD", "1Secure*Password1")
-        self.SQLSERVER_USER = user
-        self.SQLSERVER_DBNAME = dbname
+        self.password = password or environ.get("SQLSERVER_PASSWORD", "1Secure*Password1")
+        self.username = username
+        self.dbname = dbname
         self.dialect = dialect
 
     def _configure(self) -> None:
-        self.with_env("SA_PASSWORD", self.SQLSERVER_PASSWORD)
-        self.with_env("SQLSERVER_USER", self.SQLSERVER_USER)
-        self.with_env("SQLSERVER_DBNAME", self.SQLSERVER_DBNAME)
+        self.with_env("SA_PASSWORD", self.password)
+        self.with_env("SQLSERVER_USER", self.username)
+        self.with_env("SQLSERVER_DBNAME", self.dbname)
         self.with_env("ACCEPT_EULA", 'Y')
 
     def get_connection_url(self) -> str:
         return super()._create_connection_url(
-            dialect=self.dialect, username=self.SQLSERVER_USER, password=self.SQLSERVER_PASSWORD,
-            db_name=self.SQLSERVER_DBNAME, port=self.port_to_expose
+            dialect=self.dialect, username=self.username, password=self.password,
+            db_name=self.dbname, port=self.port_to_expose
         )
