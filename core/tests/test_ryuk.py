957b863f14f8ea21d5877222d3f1d8bc6b2c18e5
Vemund Santi <vemund@santi.no>
2023-03-05 00:01:28 +0100

    Add tests for ryuk cleanup

---
+++ core/tests/test_ryuk.py
@@ -0,0 +1,21 @@
+import pytest
+
+from testcontainers.core.reaper import Reaper
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.waiting_utils import wait_for_logs
+
+def test_wait_for_reaper():
+    container = DockerContainer("hello-world").with_ryuk(True).start()
+    wait_for_logs(container, "Hello from Docker!")
+    assert Reaper._socket is not None
+    Reaper._socket.close()
+
+    assert Reaper._container is not None
+    wait_for_logs(Reaper._container, r".* Removed 1 .*", timeout=15)
+
+    Reaper.delete_instance()
+
+def test_container_without_ryuk():
+    container = DockerContainer("hello-world").start()
+    wait_for_logs(container, "Hello from Docker!")
+    assert Reaper._instance is None
