dba95d90692d6efd8f5f4a75a7255895b3d021d3
Edwin Onuonga <ed@hazy.com>
2023-03-21 03:14:28 +0000

    remove platform kwarg

--- db2/tests/test_db2.py
+++ db2/tests/test_db2.py
@@ -8,7 +8,7 @@ from testcontainers.db2 import Db2Container
 @pytest.mark.skipif(is_arm(), reason='ibm_db_sa adapter not compatible with ARM64')
 def test_docker_run_db2():
     with tempfile.TemporaryDirectory() as tempdir:
-        config = Db2Container("ibmcom/db2:11.5.7.0", privileged=True, platform="linux/amd64")
+        config = Db2Container("ibmcom/db2:11.5.7.0", privileged=True)
         with config.with_volume_mapping(tempdir, "/database", mode="rw") as db2:
             engine = sqlalchemy.create_engine(db2.get_connection_url())
             with engine.connect() as conn:
