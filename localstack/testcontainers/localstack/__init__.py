dda2b4523bd2c4e94fa4ce5b5115245354457724
Till Hoffmann <tillahoffmann@gmail.com>
2023-04-13 09:29:24 -0400

    Drop lambda service in localstack doctests.

--- localstack/testcontainers/localstack/__init__.py
+++ localstack/testcontainers/localstack/__init__.py
@@ -25,17 +25,16 @@ class LocalStackContainer(DockerContainer):
             >>> from testcontainers.localstack import LocalStackContainer
 
             >>> with LocalStackContainer(image="localstack/localstack:0.11.4") as localstack:
-            ...     localstack.with_services("dynamodb", "lambda")
             ...     dynamo_endpoint = localstack.get_url()
             <testcontainers.localstack.LocalStackContainer object at 0x...>
 
         The endpoint can be used to create a client with the boto3 library:
+
         .. doctest::
 
             >>> from testcontainers.localstack import LocalStackContainer
 
             >>> with LocalStackContainer(image="localstack/localstack:0.11.4") as localstack:
-            ...     localstack.with_services("dynamodb", "lambda")
             ...     dynamo_endpoint = localstack.get_url()
             ...     dynamo_client = boto3.client("dynamodb", endpoint_url=dynamo_endpoint)
             ...     scan_result = dynamo_client.scan(TableName='foo')
