5b1988e216a27cd7ee7c883ec5d7b037947524cc
Vemund Santi <vemund@santi.no>
2023-05-23 21:10:38 +0200

    Fix bug where language label was not added when provided custom labels

--- core/testcontainers/core/labels.py
+++ core/testcontainers/core/labels.py
@@ -6,13 +6,13 @@ from .config import RYUK_IMAGE
 
 SESSION_ID: str = str(uuid4())
 LABEL_SESSION_ID = "org.testcontainers.session-id"
+LABEL_LANG = "org.testcontainers.lang"
 
 
 def create_labels(image: str, labels: Optional[dict]) -> dict:
     if labels is None:
-        labels = {
-            "org.testcontainers.lang": "python",
-        }
+        labels = {}
+    labels[LABEL_LANG] = "python"
 
     if image == RYUK_IMAGE:
         return labels
