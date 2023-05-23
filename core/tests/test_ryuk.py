8802f39624f1131c6f1b96a70d380e0f9575ea82
Vemund Santi <vemund@santi.no>
2023-05-23 14:16:43 +0200

    Update Ryuk tests to not rely on __del__ dunder for cleanup

--- core/tests/test_ryuk.py
+++ core/tests/test_ryuk.py
@@ -4,13 +4,16 @@ from testcontainers.core.waiting_utils import wait_for_logs
 
 
 def test_wait_for_reaper():
-    with DockerContainer("hello-world") as container:
-        wait_for_logs(container, "Hello from Docker!")
-        assert Reaper._socket is not None
-        Reaper._socket.close()
+    container = DockerContainer("hello-world").start()
+    wait_for_logs(container, "Hello from Docker!")
 
-        assert Reaper._container is not None
-        wait_for_logs(Reaper._container, r".* Removed 1 .*", timeout=15)
+    assert Reaper._socket is not None
+    Reaper._socket.close()
+
+    assert Reaper._container is not None
+    wait_for_logs(Reaper._container, r".* Removed 1 .*", timeout=15)
+
+    Reaper.delete_instance()
 
 
 def test_container_without_ryuk():
