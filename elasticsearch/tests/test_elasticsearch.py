e18b27f82e2886796efa2d72b66f38ebadb512cb
Vemund Santi <vemund@santi.no>
2023-05-23 10:41:45 +0200

    Update docstrings

--- elasticsearch/tests/test_elasticsearch.py
+++ elasticsearch/tests/test_elasticsearch.py
@@ -5,7 +5,7 @@ import pytest
 from testcontainers.elasticsearch import ElasticSearchContainer
 
 
-# The versions below were the current supported versions at time of writing (2022-08-11)
+# The versions below should reflect the latest stable releases of maintained versions
 @pytest.mark.parametrize('version', ['7.17.10', '8.7.1'])
 def test_docker_run_elasticsearch(version):
     with ElasticSearchContainer(f'elasticsearch:{version}') as es:
