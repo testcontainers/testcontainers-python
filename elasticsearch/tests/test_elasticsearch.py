755fafeea415b2a0b9d0e357adc4d33da95be34b
Vemund Santi <vemund@santi.no>
2023-05-23 10:06:44 +0200

    Drop unsupported ElasticSearch container. Now all tests use ARM64/v8 Docker images

--- elasticsearch/tests/test_elasticsearch.py
+++ elasticsearch/tests/test_elasticsearch.py
@@ -6,7 +6,7 @@ from testcontainers.elasticsearch import ElasticSearchContainer
 
 
 # The versions below were the current supported versions at time of writing (2022-08-11)
-@pytest.mark.parametrize('version', ['6.8.23', '7.17.5', '8.3.3'])
+@pytest.mark.parametrize('version', ['7.17.10', '8.7.1'])
 def test_docker_run_elasticsearch(version):
     with ElasticSearchContainer(f'elasticsearch:{version}') as es:
         resp = urllib.request.urlopen(es.get_url())
