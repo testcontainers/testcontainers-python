46c94f270456f2155968b210872e9ee645047839
Gerard Venzl <gerald.venzl@gmail.com>
2023-06-10 22:05:12 -0700

    Expose container environment variables

--- modules/oracle/testcontainers/oracle/__init__.py
+++ modules/oracle/testcontainers/oracle/__init__.py
@@ -1,3 +1,5 @@
+from os import environ
+from typing import Optional
 from testcontainers.core.generic import DbContainer
 
 
@@ -18,16 +20,36 @@ class OracleDbContainer(DbContainer):
             ...         result = connection.execute(sqlalchemy.text("SELECT 1 FROM dual"))
     """
 
-    def __init__(self, image: str = "gvenzl/oracle-free:slim", **kwargs) -> None:
+    def __init__(self, image: str = "gvenzl/oracle-free:slim",
+                 oracle_password: Optional[str] = None,
+                 username: Optional[str] = None, password: Optional[str] = None,
+                 port: int = 1521, dbname: Optional[str] = None, **kwargs) -> None:
         super(OracleDbContainer, self).__init__(image=image, **kwargs)
-        self.container_port = 1521
-        self.with_exposed_ports(self.container_port)
+
+        self.port = port
+        self.with_exposed_ports((self.port,))
+
+        self.oracle_password = oracle_password or environ.get("ORACLE_PASSWORD")
+        self.username = username or environ.get("APP_USER")
+        self.password = password or environ.get("APP_USER_PASSWORD")
+        self.dbname = dbname or environ.get("ORACLE_DATABASE")
 
     def get_connection_url(self) -> str:
         return super()._create_connection_url(
-            dialect="oracle+oracledb", username="system", password="oracle",
-            port=self.container_port, dbname="freepdb1"
+            dialect="oracle+oracledb",
+            username=self.username or "system", password=self.password or self.oracle_password,
+            port=self.port, dbname=self.dbname
         )
 
     def _configure(self) -> None:
-        pass
+        if self.oracle_password is not None:
+            self.with_env("ORACLE_PASSWORD", self.oracle_password)
+        else:
+            self.with_env("ORACLE_RANDOM_PASSWORD", "y")
+
+        if self.username is not None:
+            self.with_env("APP_USER", self.username)
+        if self.password is not None:
+            self.with_env("APP_USER_PASSWORD", self.password)
+        if self.dbname is not None:
+            self.with_env("ORACLE_DATABASE", self.dbname)
