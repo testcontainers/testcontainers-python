756a6dc86b708bc3a375c85794093c77442ae1fc
Gerard Venzl <gerald.venzl@gmail.com>
2023-06-10 19:52:13 -0700

    Update Oracle DB version
    Signed-off-by: gvenzl <gerald.venzl@gmail.com>

--- modules/oracle/testcontainers/oracle/__init__.py
+++ modules/oracle/testcontainers/oracle/__init__.py
@@ -15,18 +15,18 @@ class OracleDbContainer(DbContainer):
             >>> with OracleDbContainer() as oracle:
             ...     engine = sqlalchemy.create_engine(oracle.get_connection_url())
             ...     with engine.begin() as connection:
-            ...         result = connection.execute(sqlalchemy.text("select * from V$VERSION"))
+            ...         result = connection.execute(sqlalchemy.text("SELECT 1 FROM dual"))
     """
 
-    def __init__(self, image: str = "wnameless/oracle-xe-11g-r2:latest", **kwargs) -> None:
-        super().__init__(image=image, **kwargs)
+    def __init__(self, image: str = "gvenzl/oracle-free:slim", **kwargs) -> None:
+        super(OracleDbContainer, self).__init__(image=image, **kwargs)
         self.container_port = 1521
         self.with_exposed_ports(self.container_port)
-        self.with_env("ORACLE_ALLOW_REMOTE", "true")
 
     def get_connection_url(self) -> str:
         return super()._create_connection_url(
-            dialect="oracle", username="system", password="oracle", port=self.container_port, dbname="xe"
+            dialect="oracle", username="system", password="oracle", port=self.container_port,
+            dbname="freepdb1"
         )
 
     def _configure(self) -> None:
