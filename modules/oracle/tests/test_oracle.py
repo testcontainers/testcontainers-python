34f1b5265285e0869168b7874c0f7c7ac064c045
Gerard Venzl <gerald.venzl@gmail.com>
2023-06-10 19:49:50 -0700

    Make test version independent
    Signed-off-by: gvenzl <gerald.venzl@gmail.com>

--- modules/oracle/tests/test_oracle.py
+++ modules/oracle/tests/test_oracle.py
@@ -5,15 +5,9 @@ from testcontainers.oracle import OracleDbContainer
 
 
 def test_docker_run_oracle():
-    versions = {
-        "Oracle Database 11g Express Edition Release 11.2.0.2.0 - 64bit Production",
-        "PL/SQL Release 11.2.0.2.0 - Production",
-        "CORE\t11.2.0.2.0\tProduction",
-        "TNS for Linux: Version 11.2.0.2.0 - Production",
-        "NLSRTL Version 11.2.0.2.0 - Production",
-    }
     with OracleDbContainer() as oracledb:
         engine = sqlalchemy.create_engine(oracledb.get_connection_url())
         with engine.begin() as connection:
-            result = connection.execute(sqlalchemy.text("select * from V$VERSION"))
-            assert {row[0] for row in result} == versions
+            test_val = 1
+            result = connection.execute(sqlalchemy.text("SELECT {} FROM dual".format(test_val)))
+            assert {row[0] for row in result} == test_val
