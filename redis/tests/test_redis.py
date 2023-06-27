fafd2b378716ab79610712eee47767eacaab4ae0
Vemund Santi <vemund@santi.no>
2023-06-27 09:45:53 +0200

    Simplify Redis tests
    Prevous tests were dependent on a lot of Redis client logic, which is not the goal
    of this package. This package provides a container with a running Redis version, how
    you interact with it is up to the user. Changed to simpler test with only basic
    client usage to make sure container is started and ready to accept connections.

--- redis/tests/test_redis.py
+++ redis/tests/test_redis.py
@@ -1,37 +1,12 @@
-import time
-from testcontainers.core.waiting_utils import wait_for_logs
+import pytest
 
 from testcontainers.redis import RedisContainer
 
 
-def test_docker_run_redis():
-    with RedisContainer('redis:7.0.11') as redis:
-        wait_for_logs(redis, 'Ready to accept connections')
-        client = redis.get_client()
-        pubsub = client.pubsub()
-        pubsub.subscribe('test')
-        client.publish('test', 'new_msg')
-        msg = wait_for_message(pubsub)
-        assert 'data' in msg
-        assert b'new_msg', msg['data']
-
-
-def test_docker_run_redis_with_password():
-    with RedisContainer('redis:7.0.11', password="mypass") as redis:
-        wait_for_logs(redis, 'Ready to accept connections')
+@pytest.mark.parametrize('version', ['7.0.11', '6.2.12'])
+def test_redis_container(version: str):
+    image_name = f'redis:{version}'
+    with RedisContainer(image_name, password="mypass") as redis:
         client = redis.get_client(decode_responses=True)
         client.set("hello", "world")
         assert client.get("hello") == "world"
-
-
-def wait_for_message(pubsub, timeout=1, ignore_subscribe_messages=True):
-    now = time.time()
-    timeout = now + timeout
-    while now < timeout:
-        message = pubsub.get_message(
-            ignore_subscribe_messages=ignore_subscribe_messages)
-        if message is not None:
-            return message
-        time.sleep(0.01)
-        now = time.time()
-    return None
