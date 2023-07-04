fefa69bfa09fe0eaaddcc988eda264784623765c
Dee Moore <dee42moore@gmail.com>
2023-07-04 11:36:23 +0200

    Remove xfail, this now works

--- core/tests/test_docker_in_docker.py
+++ core/tests/test_docker_in_docker.py
@@ -4,7 +4,6 @@ from testcontainers.core.docker_client import DockerClient
 from testcontainers.core.waiting_utils import wait_for_logs
 
 
-@pytest.mark.xfail(reason="https://github.com/docker/docker-py/issues/2717")
 def test_wait_for_logs_docker_in_docker():
     # real dind isn't possible (AFAIK) in CI
     # forwarding the socket to a container port is at least somewhat the same
