59eefebe52f6c0b72144e15e1dc21ceb424489dd
TheForgotten <angelo.mf.paiva@gmail.com>
2023-07-06 14:33:03 +0100

    feat: Update KeycloakContainer to be compatible with new image

--- keycloak/testcontainers/keycloak/__init__.py
+++ keycloak/testcontainers/keycloak/__init__.py
@@ -11,44 +11,75 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 import os
-import requests
-
-from keycloak import KeycloakAdmin
-
-from testcontainers.core.container import DockerContainer
-from testcontainers.core.waiting_utils import wait_container_is_ready
 from typing import Optional
+
+import requests
+from keycloak import KeycloakAdmin
+from testcontainers.core.container import DockerContainer
+from testcontainers.core.waiting_utils import wait_container_is_ready, wait_for_logs
 
 
 class KeycloakContainer(DockerContainer):
     """
-    Keycloak container.
+        Keycloak container.
 
-    Example:
+        Example:
 
-        .. doctest::
+            .. doctest::
 
-            >>> from testcontainers.keycloak import KeycloakContainer
+                >>> from testcontainers.keycloak import KeycloakContainer
 
-            >>> with KeycloakContainer() as kc:
-            ...    keycloak = kc.get_client()
+                >>> with KeycloakContainer() as kc:
+                ...    keycloak = kc.get_client()
     """
-    def __init__(self, image="jboss/keycloak:latest", username: Optional[str] = None,
-                 password: Optional[str] = None, port: int = 8080) -> None:
+    def __init__(
+            self,
+            image="quay.io/keycloak/keycloak:latest",
+            username: Optional[str] = None,
+            password: Optional[str] = None,
+            port: int = 8080
+    ) -> None:
         super(KeycloakContainer, self).__init__(image=image)
-        self.username = username or os.environ.get("KEYCLOAK_USER", "test")
-        self.password = password or os.environ.get("KEYCLOAK_PASSWORD", "test")
+        self.username = username or os.getenv("KEYCLOAK_ADMIN", "test")
+        self.password = password or os.getenv("KEYCLOAK_ADMIN_PASSWORD", "test")
         self.port = port
         self.with_exposed_ports(self.port)
 
+    def _get_image_tag(self) -> str:
+        return self.image.split(":")[1]
+
+    def _uses_legacy_architecture(self) -> bool:
+        tag = self._get_image_tag()
+
+        try:
+            major_version_number = int(tag.split(".")[0])
+        except ValueError:
+            major_version_number = 2023
+
+        return major_version_number <= 16 or "legacy" in tag.lower()
+
     def _configure(self) -> None:
+        # There's a bit of confusion as to which version / architecture uses which
+        # Because of this we're setting the credentials both ways
+        self.with_env("KEYCLOAK_ADMIN", self.username)
+        self.with_env("KEYCLOAK_ADMIN_PASSWORD", self.password)
         self.with_env("KEYCLOAK_USER", self.username)
         self.with_env("KEYCLOAK_PASSWORD", self.password)
+
+        self.with_env("KC_HEALTH_ENABLED", "true")
+        self.with_env("KC_METRICS_ENABLED", "true")
+
+        if not self._uses_legacy_architecture():
+            self.with_command("start-dev")
 
     def get_url(self) -> str:
         host = self.get_container_host_ip()
         port = self.get_exposed_port(self.port)
         return f"http://{host}:{port}"
+
+    def get_base_api_url(self) -> str:
+        base_url = self.get_url()
+        return f"{base_url}/auth" if self._uses_legacy_architecture() else base_url
 
     @wait_container_is_ready(requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout)
     def _connect(self) -> None:
@@ -58,20 +89,19 @@ class KeycloakContainer(DockerContainer):
 
     def start(self) -> "KeycloakContainer":
         self._configure()
-        super().start()
-        self._connect()
+        container = super().start()
+
+        if self._uses_legacy_architecture():
+            self._connect()
+        else:
+            wait_for_logs(container, 'Listening on:')
+
         return self
 
     def get_client(self, **kwargs) -> KeycloakAdmin:
-        default_kwargs = dict(
-            server_url=f"{self.get_url()}/auth/",
+        return KeycloakAdmin(
+            server_url=f"{self.get_base_api_url()}/",
             username=self.username,
             password=self.password,
-            realm_name="master",
-            verify=True,
+            verify=True
         )
-        kwargs = {
-            **default_kwargs,
-            **kwargs
-        }
-        return KeycloakAdmin(**kwargs)
