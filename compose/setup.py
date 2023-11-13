2bd20c6a9ab811343beee150a4816711848fb9ea
Dave Ankin <daveankin@gmail.com>
2023-11-13 06:18:08 -0500

    #392: adding comment to explain why testcontainers-compose no longer depends on docker-compose

--- compose/setup.py
+++ compose/setup.py
@@ -12,6 +12,7 @@ setup(
     url="https://github.com/testcontainers/testcontainers-python",
     install_requires=[
         "testcontainers-core",
+        # docker compose support has been removed to fix dependency issues in the build
         # "docker-compose",
     ],
     python_requires=">=3.7",
