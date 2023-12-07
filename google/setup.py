151a5d44370ec20bd5feb0163c151422630d4033
barendlinders <blinders@nicolab.com>
2023-12-07 10:29:23 +0100

    Release new version of testcontainers-gcp
    No code changes have been made, but current `main` is ahead of PyPI.
    See  https://github.com/testcontainers/testcontainers-python/issues/403

--- google/setup.py
+++ google/setup.py
@@ -4,7 +4,7 @@ description = "Google Cloud Platform component of testcontainers-python."
 
 setup(
     name="testcontainers-gcp",
-    version="0.0.1rc1",
+    version="0.0.2rc1",
     packages=find_namespace_packages(),
     description=description,
     long_description=description,
