af4c18a10fb0a1b55acf91eb58c59a2d7d8b8de7
Luc Sorel-Giffo <luc.sorel@gmail.com>
2024-01-16 16:48:19 +0100

    refactor(influxdb): remove unused imports, order imports

--- modules/influxdb/tests/test_influxdb.py
+++ modules/influxdb/tests/test_influxdb.py
@@ -1,11 +1,23 @@
-from datetime import datetime
-from dateutil.parser import parse
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
 from typing import Type
 
+from dateutil.parser import parse
 from influxdb.resultset import ResultSet
 from influxdb_client import Bucket
-from pytest import mark
 from influxdb_client.client.write_api import SYNCHRONOUS
+from pytest import mark
 
 from testcontainers.influxdb import InfluxDbContainer
 from testcontainers.influxdb1 import InfluxDb1Container
