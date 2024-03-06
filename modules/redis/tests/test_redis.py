cc4cb3762802dc75b0801727d8b1f1a1c56b7f50
Barrett Strausser <bearrito@users.noreply.github.com>
2024-03-06 11:11:34 -0500

    feat(redis): support AsyncRedisContainer (#442)
    Co-authored-by: bstrausser <bstrausser@locusrobotics.com>

--- modules/redis/tests/test_redis.py
+++ modules/redis/tests/test_redis.py
@@ -1,6 +1,7 @@
 import time
 
-from testcontainers.redis import RedisContainer
+from testcontainers.redis import RedisContainer, AsyncRedisContainer
+import pytest
 
 
 def test_docker_run_redis():
@@ -21,6 +22,49 @@ def test_docker_run_redis_with_password():
         client = redis.get_client(decode_responses=True)
         client.set("hello", "world")
         assert client.get("hello") == "world"
+
+
+pytest.mark.usefixtures("anyio_backend")
+
+
+@pytest.mark.parametrize("anyio_backend", ["asyncio"])
+async def test_key_set_in_async_redis(anyio_backend):
+    with AsyncRedisContainer() as container:
+        async_redis_client: redis.Redis = await container.get_async_client(decode_responses=True)
+        key = "key"
+        expected_value = 1
+        await async_redis_client.set(key, expected_value)
+        actual_value = await async_redis_client.get(key)
+        assert int(actual_value) == expected_value
+
+
+pytest.mark.usefixtures("anyio_backend")
+
+
+@pytest.mark.parametrize("anyio_backend", ["asyncio"])
+@pytest.mark.skip(reason="Need to sort out async pub/sub")
+async def test_docker_run_async_redis(anyio_backend):
+    config = AsyncRedisContainer()
+    with config as container:
+        client: redis.Redis = await container.get_async_client(decode_responses=True)
+        p = await client.pubsub()
+        await p.subscribe("test")
+        await client.publish("test", "new_msg")
+        msg = wait_for_message(p)
+        assert "data" in msg
+        assert b"new_msg", msg["data"]
+
+
+pytest.mark.usefixtures("anyio_backend")
+
+
+@pytest.mark.parametrize("anyio_backend", ["asyncio"])
+async def test_docker_run_async_redis_with_password(anyio_backend):
+    config = AsyncRedisContainer(password="mypass")
+    with config as container:
+        client: redis.Redis = await container.get_async_client(decode_responses=True)
+        await client.set("hello", "world")
+        assert await client.get("hello") == "world"
 
 
 def wait_for_message(pubsub, timeout=1, ignore_subscribe_messages=True):
