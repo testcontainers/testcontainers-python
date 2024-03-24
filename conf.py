dfd17814a7fc9ede510ae17569004bd92f2a6fa6
David Ankin <daveankin@gmail.com>
2024-03-23 23:42:27 -0400

    fix: read the docs build works again (#496)
    proof of working build -
    https://readthedocs.org/projects/testcontainers/builds/23851186/
    
    this is the one i created - "testcontainers" - the one we actually link
    to is "testcontainers-python"
    
    not sure how to update to re-run that one, which is what we want to do
    (and i will either rename or delete the one i just made).

--- conf.py
+++ conf.py
@@ -48,7 +48,7 @@ templates_path = ["_templates"]
 source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = "INDEX"
+master_doc = "index"
 
 # General information about the project.
 project = "testcontainers"
