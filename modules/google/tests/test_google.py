b04391ef06443e1db073a4f38901e41ae39e6e6b
Matt Oates <matt.oates@biorelate.com>
2024-03-26 22:52:01 +0000

    feat(google): add support for Datastore emulator

--- modules/google/tests/test_google.py
+++ modules/google/tests/test_google.py
@@ -1,7 +1,8 @@
 from queue import Queue
+from google.cloud.datastore import Entity
 
 from testcontainers.core.waiting_utils import wait_for_logs
-from testcontainers.google import PubSubContainer
+from testcontainers.google import PubSubContainer, DatastoreContainer
 
 
 def test_pubsub_container():
@@ -27,3 +28,47 @@ def test_pubsub_container():
         message = queue.get(timeout=1)
         assert message.data == b"Hello world!"
         message.ack()
+
+
+def test_datastore_container_creation():
+    # Initialize the Datastore emulator container
+    with DatastoreContainer() as datastore:
+        # Obtain a datastore client configured to connect to the emulator
+        client = datastore.get_datastore_client()
+
+        # Define a unique key for a test entity to ensure test isolation
+        key = client.key("TestKind", "test_id_1")
+
+        # Create and insert a new entity
+        entity = Entity(key=key)
+        entity.update({"foo": "bar"})
+        client.put(entity)
+
+        # Fetch the just-inserted entity directly
+        fetched_entity = client.get(key)
+
+        # Assert that the fetched entity matches what was inserted
+        assert fetched_entity is not None, "Entity was not found in the datastore."
+        assert fetched_entity["foo"] == "bar", "Entity attribute 'foo' did not match expected value 'bar'."
+
+
+def test_datastore_container_isolation():
+    # Initialize the Datastore emulator container
+    with DatastoreContainer() as datastore:
+        # Obtain a datastore client configured to connect to the emulator
+        client = datastore.get_datastore_client()
+
+        # Define a unique key for a test entity to ensure test isolation
+        key = client.key("TestKind", "test_id_1")
+
+        # Create and insert a new entity
+        entity = Entity(key=key)
+        entity.update({"foo": "bar"})
+        client.put(entity)
+
+        # Create a second container and try to fetch the entity to makesure its a different container
+        with DatastoreContainer() as datastore2:
+            assert datastore.get_datastore_emulator_host() != datastore2.get_datastore_emulator_host(), "Datastore containers use the same port."
+            client2 = datastore2.get_datastore_client()
+            fetched_entity2 = client2.get(key)
+            assert fetched_entity2 is None, "Entity was found in the datastore."
