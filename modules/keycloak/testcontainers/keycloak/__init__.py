de25c7ee384a0c25d0377bf322b4351f3bc3af52
Vemund Santi <vemund@santi.no>
2024-03-27 12:59:13 +0100

    Add missing newline to make ruff happy

--- modules/keycloak/testcontainers/keycloak/__init__.py
+++ modules/keycloak/testcontainers/keycloak/__init__.py
@@ -19,7 +19,6 @@ from keycloak import KeycloakAdmin
 from testcontainers.core.container import DockerContainer
 from testcontainers.core.waiting_utils import wait_container_is_ready, wait_for_logs
 
-
 _DEFAULT_DEV_COMMAND = "start-dev"
 
 
