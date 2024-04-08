72d8e19fd9328cdeb3b88b48e1b5caa78939fe3b
David Ankin <daveankin@gmail.com>
2024-04-08 04:33:29 -0400

    editable config from main config module

--- core/testcontainers/core/config.py
+++ core/testcontainers/core/config.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from functools import cached_property
 from os import environ
 
 MAX_TRIES = int(environ.get("TC_MAX_TRIES", 120))
@@ -9,3 +11,23 @@ RYUK_PRIVILEGED: bool = environ.get("TESTCONTAINERS_RYUK_PRIVILEGED", "false") =
 RYUK_DISABLED: bool = environ.get("TESTCONTAINERS_RYUK_DISABLED", "false") == "true"
 RYUK_DOCKER_SOCKET: str = environ.get("TESTCONTAINERS_DOCKER_SOCKET_OVERRIDE", "/var/run/docker.sock")
 RYUK_RECONNECTION_TIMEOUT: str = environ.get("RYUK_RECONNECTION_TIMEOUT", "10s")
+
+
+@dataclass
+class TestcontainersConfiguration:
+    max_tries: int = MAX_TRIES
+    sleep_time: int = SLEEP_TIME
+    ryuk_image: str = RYUK_IMAGE
+    ryuk_privileged: bool = RYUK_PRIVILEGED
+    ryuk_disabled: bool = RYUK_DISABLED
+    ryuk_docker_socket: str = RYUK_DOCKER_SOCKET
+    ryuk_reconnection_timeout: str = RYUK_RECONNECTION_TIMEOUT
+
+    @cached_property
+    def timeout(self):
+        return self.max_tries * self.sleep_time
+
+
+testcontainers_config = TestcontainersConfiguration()
+
+__all__ = ["testcontainers_config"]
