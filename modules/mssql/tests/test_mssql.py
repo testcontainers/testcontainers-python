045d29b7bafc58defa387c9d85acfa85f1830ccb
Lyndon Fan <lyndon0808@gmail.com>
2024-04-19 19:59:49 +0100

    test: test quoted password for mssql

--- modules/mssql/tests/test_mssql.py
+++ modules/mssql/tests/test_mssql.py
@@ -23,3 +23,35 @@ def test_docker_run_azure_sql_edge():
             result = connection.execute(sqlalchemy.text("select @@servicename"))
             for row in result:
                 assert row[0] == "MSSQLSERVER"
+
+
+# This is a feature in the generic DbContainer class
+# but it can't be tested on its own
+# so is tested in various database modules:
+# - mysql / mariadb
+# - postgresql
+# - sqlserver
+# - oracle
+def test_quoted_password():
+    user = "SA"
+    # spaces seem to cause issues?
+    password = "p@$%25+0&%rd:/!=?"
+    quoted_password = "p%40%24%2525+0%26%25rd%3A%2F%21%3D%3F"
+    driver = "pymssql"
+    port = 1433
+    expected_url = f"mssql+{driver}://{user}:{quoted_password}@localhost:{port}/tempdb"
+    kwargs = {
+        "username": user,
+        "password": password,
+    }
+    with (
+        SqlServerContainer("mcr.microsoft.com/azure-sql-edge:1.0.7", **kwargs)
+        .with_env("ACCEPT_EULA", "Y")
+        .with_env(
+            "MSSQL_SA_PASSWORD", "{" + password + "}"
+        )  # special characters have to be quoted in braces in env vars
+    ) as container:
+        exposed_port = container.get_exposed_port(container.port)
+        expected_url = expected_url.replace(f":{port}", f":{exposed_port}")
+        url = container.get_connection_url()
+        assert url == expected_url
