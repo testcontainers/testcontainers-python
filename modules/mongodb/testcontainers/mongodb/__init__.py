92d2c63388d4052e9ef73b4a5b433bb0f0bb4ff7
David Ankin <daveankin@gmail.com>
2024-04-23 13:36:57 -0400

    mongodb replicaSet rs.initiate()

--- modules/mongodb/testcontainers/mongodb/__init__.py
+++ modules/mongodb/testcontainers/mongodb/__init__.py
@@ -45,6 +45,7 @@ class MongoDbContainer(DbContainer):
             ...    result["restaurant_id"]
             '123456'
     """
+    AWAIT_INIT_REPLICA_SET_ATTEMPTS = 60
 
     def __init__(
         self,
@@ -53,6 +54,7 @@ class MongoDbContainer(DbContainer):
         username: Optional[str] = None,
         password: Optional[str] = None,
         dbname: Optional[str] = None,
+        sharding_enabled: bool = True,
         **kwargs,
     ) -> None:
         raise_for_deprecated_parameter(kwargs, "port_to_expose", "port")
@@ -61,6 +63,7 @@ class MongoDbContainer(DbContainer):
         self.password = password if password else os.environ.get("MONGO_INITDB_ROOT_PASSWORD", "test")
         self.dbname = dbname if dbname else os.environ.get("MONGO_DB", "test")
         self.port = port
+        self.sharding_enabled = sharding_enabled
         self.with_exposed_ports(self.port)
 
     def _configure(self) -> None:
@@ -78,6 +81,30 @@ class MongoDbContainer(DbContainer):
 
     def _connect(self) -> None:
         wait_for_logs(self, "Waiting for connections")
+
+    def start(self) -> "MongoDbContainer":
+        super().start()
+        if self.sharding_enabled:
+            self._init_replica_set()
+        return self
+
+    def _init_replica_set(self):
+        cmd = "rs.initiate()"
+        mongo_cmd = f"mongosh mongo --eval \"{cmd}\" || mongo --eval \"{cmd}\""
+        self.exec(command=["sh", "-c", mongo_cmd])
+
+        cmd = ("var attempt = 0; "
+               "while (db.runCommand( { isMaster: 1 } ).ismaster==false) {"
+               f"  if (attempt > {self.AWAIT_INIT_REPLICA_SET_ATTEMPTS}) {'{ quit(1); }'}"
+               "  print('%s ', attempt); sleep(100); attempt++;"
+               "}")
+        mongo_cmd = f"mongosh mongo --eval \"{cmd}\" || mongo --eval \"{cmd}\""
+        status, _ = self.exec(command=["sh", "-c", mongo_cmd])
+
+        assert status == 0, f"A single node replica set was not initialized in a set timeout: {self.AWAIT_INIT_REPLICA_SET_ATTEMPTS} attempts"
+
+    def get_replica_set_url(self):
+        raise Exception("not implemented yet")
 
     def get_connection_client(self) -> MongoClient:
         return MongoClient(self.get_connection_url())
