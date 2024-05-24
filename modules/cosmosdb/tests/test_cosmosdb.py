92098111fe77c4e03701e99a51663fda20517f62
David Ankin <daveankin@gmail.com>
2024-05-24 17:40:40 -0400

    linting

--- modules/cosmosdb/tests/test_cosmosdb.py
+++ modules/cosmosdb/tests/test_cosmosdb.py
@@ -1,6 +1,7 @@
 import pytest
 from testcontainers.cosmosdb import CosmosDBEmulatorContainer
 
+
 def test_docker_run():
-	with CosmosDBEmulatorContainer(partition_count=1) as cosmosdb:
-		list(cosmosdb.sync_client().list_databases())
+    with CosmosDBEmulatorContainer(partition_count=1) as cosmosdb:
+        list(cosmosdb.sync_client().list_databases())
