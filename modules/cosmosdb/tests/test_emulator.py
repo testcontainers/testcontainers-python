ff6f04f624f293e87f83214fefa775251f5a96d7
Mehdi BEN ABDALLAH <@mbenabda>
2024-05-25 23:16:38 +0200

    wip

---
+++ modules/cosmosdb/tests/test_emulator.py
@@ -0,0 +1,7 @@
+import pytest
+from testcontainers.cosmosdb._emulator import CosmosDBEmulatorContainer
+
+def test_runs():
+    with CosmosDBEmulatorContainer(partition_count=1, bind_ports=False) as emulator:
+        assert emulator.server_certificate_pem is not None
+        assert emulator.get_exposed_port(8081) is not None
