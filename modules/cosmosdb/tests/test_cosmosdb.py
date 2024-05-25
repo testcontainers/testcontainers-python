eea22ccb23e82e7f457bd4977af6875686a99d36
Mehdi BEN ABDALLAH <@mbenabda>
2024-05-25 15:13:49 +0200

    actually expose the MongoDB endpoint when requested

--- modules/cosmosdb/tests/test_cosmosdb.py
+++ modules/cosmosdb/tests/test_cosmosdb.py
@@ -1,7 +1,21 @@
 import pytest
-from testcontainers.cosmosdb import CosmosDBEmulatorContainer
+from testcontainers.cosmosdb import CosmosDBEmulatorContainer, Endpoints
 
 
 def test_docker_run():
     with CosmosDBEmulatorContainer(partition_count=1) as cosmosdb:
         list(cosmosdb.sync_client().list_databases())
+
+
+def test_enabling_mondogb_endpoint_requires_a_version():
+    with pytest.raises(AssertionError, match="A MongoDB version is required to use the MongoDB Endpoint"):
+        CosmosDBEmulatorContainer(endpoints=[Endpoints.MongoDB])
+
+    # instanciates
+    CosmosDBEmulatorContainer(endpoints=[Endpoints.MongoDB], mongodb_version="4.0")
+
+
+def test_enables_mondogb_endpoint():
+    with CosmosDBEmulatorContainer(partition_count=1, endpoints=[Endpoints.MongoDB], mongodb_version="4.0") as emulator:
+        assert emulator.env["AZURE_COSMOS_EMULATOR_ENABLE_MONGODB_ENDPOINT"] == "4.0"
+        assert emulator.get_exposed_port(10255) is not None, "The MongoDB endpoint's port should be exposed"
