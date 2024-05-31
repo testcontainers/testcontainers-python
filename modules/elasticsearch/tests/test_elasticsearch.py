d9ad323cd0228255183bf77f11f7cac7a3ef943d
Joey Jurjens <joeyjurjens@gmail.com>
2024-05-31 20:38:51 +0200

    Add failing test

--- modules/elasticsearch/tests/test_elasticsearch.py
+++ modules/elasticsearch/tests/test_elasticsearch.py
@@ -12,3 +12,10 @@ def test_docker_run_elasticsearch(version):
     with ElasticSearchContainer(f"elasticsearch:{version}", mem_limit="3G") as es:
         resp = urllib.request.urlopen(es.get_url())
         assert json.loads(resp.read().decode())["version"]["number"] == version
+
+
+@pytest.mark.parametrize("version", ["7.17.18", "8.12.2"])
+def test_docker_run_elasticsearch_custom_port(version):
+    with ElasticSearchContainer(f"elasticsearch:{version}", mem_limit="3G", port=9876) as es:
+        resp = urllib.request.urlopen(es.get_url())
+        assert json.loads(resp.read().decode())["version"]["number"] == version
