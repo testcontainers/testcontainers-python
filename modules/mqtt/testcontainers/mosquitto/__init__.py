6f68594b7f2a7754aa5ea9237328382930c91d35
f18m <francesco.montorsi@gmail.com>
2024-06-07 23:48:41 +0200

    simplify PR

--- modules/mqtt/testcontainers/mosquitto/__init__.py
+++ modules/mqtt/testcontainers/mosquitto/__init__.py
@@ -71,9 +71,6 @@ class MosquittoContainer(DockerContainer):
         # helper used to turn asynchronous methods into synchronous:
         self.msg_queue = Queue()
 
-        # dictionary of watched topics and their message counts:
-        self.watched_topics = {}
-
         # reusable client context:
         self.client = None
 
@@ -109,7 +106,6 @@ class MosquittoContainer(DockerContainer):
 
             # connect() is a blocking call:
             err = self.client.connect(self.get_container_host_ip(), int(self.get_exposed_port(self.port)))
-            self.client.on_message = MosquittoContainer.on_message
             self.client.loop_start()  # launch a thread to call loop() and dequeue the message
 
         return self.client, err
@@ -122,108 +118,10 @@ class MosquittoContainer(DockerContainer):
     def stop(self, force=True, delete_volume=True) -> None:
         self.client.disconnect()
         self.client = None  # force recreation of the client object at next start()
-        self.watched_topics = {}  # clean all watched topics as well
         super().stop(force, delete_volume)
 
-    class WatchedTopicInfo:
-        def __init__(self):
-            self.count = 0
-            self.timestamp_start_watch = time.time()
-            self.last_payload = ""
-
-        def on_message(self, msg: mqtt_client.MQTTMessage):
-            self.count += 1
-            # for simplicity: assume strings are used in integration tests and are UTF8-encoded:
-            self.last_payload = msg.payload.decode("UTF-8")
-
-        def get_count(self):
-            return self.count
-
-        def get_last_payload(self):
-            return self.last_payload
-
-        def get_rate(self):
-            duration = time.time() - self.timestamp_start_watch
-            if duration > 0:
-                return self.count / duration
-            return 0
-
-    def on_message(client: mqtt_client.Client, mosquitto_container: "MosquittoContainer", msg: mqtt_client.MQTTMessage):
-        # very verbose but useful for debug:
-        # print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-        if msg.topic == "$SYS/broker/messages/received":
-            mosquitto_container.msg_queue.put(msg)
-        else:
-            # this should be a topic added through the watch_topics() API...
-            # just check it has not been removed (e.g. by unwatch_all):
-            if msg.topic in mosquitto_container.watched_topics:
-                mosquitto_container.watched_topics[msg.topic].on_message(msg)
-            else:
-                print(f"Received msg on topic [{msg.topic}] that is not being watched")
-
-    def get_messages_received(self) -> int:
-        """
-        Returns the total number of messages received by the broker so far.
-        """
-
-        client, err = self.get_client()
-        if not client.is_connected():
-            raise RuntimeError(f"Could not connect to Mosquitto broker: {err}")
-
-        client.subscribe("$SYS/broker/messages/received")
-
-        # wait till we get the first message from the topic;
-        # this wait will be up to 'sys_interval' second long (see mosquitto.conf)
-        try:
-            message = self.msg_queue.get(block=True, timeout=5)
-            return int(message.payload.decode())
-        except Queue.Empty:
-            return 0
-
-    def watch_topics(self, topics: list):
-        client, err = self.get_client()
-        if not client.is_connected():
-            raise RuntimeError(f"Could not connect to Mosquitto broker: {err}")
-
-        filtered_topics = []
-        for t in topics:
-            if t in self.watched_topics:
-                continue  # nothing to do... the topic had already been subscribed
-            self.watched_topics[t] = MosquittoContainer.WatchedTopicInfo()
-            # the topic list is actually a list of tuples (topic_name,qos)
-            filtered_topics.append((t, 0))
-
-        # after subscribe() the on_message() callback will be invoked
-        err, _ = client.subscribe(filtered_topics)
-        if err != paho.mqtt.enums.MQTTErrorCode.MQTT_ERR_SUCCESS:
-            raise RuntimeError(f"Failed to subscribe to topics: {filtered_topics}")
-
-    def unwatch_all(self):
-        client, err = self.get_client()
-        if not client.is_connected():
-            raise RuntimeError(f"Could not connect to Mosquitto broker: {err}")
-
-        # unsubscribe from all topics
-        client.unsubscribe(list(self.watched_topics.keys()))
-        self.watched_topics = {}
-
-    def get_messages_received_in_watched_topic(self, topic: str) -> int:
-        if topic not in self.watched_topics:
-            raise RuntimeError(f"Topic {topic} is not watched! Fix the test")
-        return self.watched_topics[topic].get_count()
-
-    def get_last_payload_received_in_watched_topic(self, topic: str) -> int:
-        if topic not in self.watched_topics:
-            raise RuntimeError(f"Topic {topic} is not watched! Fix the test")
-        return self.watched_topics[topic].get_last_payload()
-
-    def get_message_rate_in_watched_topic(self, topic: str) -> int:
-        if topic not in self.watched_topics:
-            raise RuntimeError(f"Topic {topic} is not watched! Fix the test")
-        return self.watched_topics[topic].get_rate()
-
-    def publish_message(self, topic: str, payload: str):
+    def publish_message(self, topic: str, payload: str, timeout: int = 2) -> None:
         ret = self.client.publish(topic, payload)
-        ret.wait_for_publish(timeout=2)
+        ret.wait_for_publish(timeout=timeout)
         if not ret.is_published():
             raise RuntimeError(f"Could not publish a message on topic {topic} to Mosquitto broker: {ret}")
