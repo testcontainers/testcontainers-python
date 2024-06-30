9f86ffb3860de51282bf0d649aa7661468ed02a1
Oliver Lambson <oliverlambson@gmail.com>
2024-06-30 13:13:08 +0100

    fix: future annotations

--- modules/mailpit/testcontainers/mailpit/__init__.py
+++ modules/mailpit/testcontainers/mailpit/__init__.py
@@ -10,6 +10,8 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+from __future__ import annotations
+
 import os
 import tempfile
 from datetime import datetime, timedelta, timezone
