c60b44c2ca40f91d0d0fa6fb6ca2342ba16c5c76
David Ankin <daveankin@gmail.com>
2024-08-16 11:27:13 -0400

    lint

--- modules/mssql/testcontainers/mssql/__init__.py
+++ modules/mssql/testcontainers/mssql/__init__.py
@@ -52,7 +52,9 @@ class SqlServerContainer(DbContainer):
 
     @wait_container_is_ready(AssertionError)
     def _connect(self) -> None:
-        status, _ = self.exec(["bash", "-c", f"/opt/mssql-tools*/bin/sqlcmd -U {self.username} -P {self.password} -Q 'SELECT 1' -C"])
+        status, _ = self.exec(
+            ["bash", "-c", f"/opt/mssql-tools*/bin/sqlcmd -U {self.username} -P {self.password} -Q 'SELECT 1' -C"]
+        )
         assert status == 0, "Cannot run 'SELECT 1': container is not ready"
 
     def get_connection_url(self) -> str:
