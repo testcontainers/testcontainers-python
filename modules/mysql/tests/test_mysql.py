9cb5986fc72410183b12931ed640d891ad3ddc67
Ivan Belyaev <vanobel159@gmail.com>
2024-11-19 00:23:39 +0300

    fix tests

--- modules/mysql/tests/test_mysql.py
+++ modules/mysql/tests/test_mysql.py
@@ -13,7 +13,12 @@ from testcontainers.mysql import MySqlContainer
 def test_docker_run_mysql():
     config = MySqlContainer("mysql:8.3.0")
     with config as mysql:
-        engine = sqlalchemy.create_engine(mysql.get_connection_url())
+        connection_url = mysql.get_connection_url()
+
+        assert mysql.dialect is None
+        assert connection_url.startswith("mysql://")
+
+        engine = sqlalchemy.create_engine(connection_url)
         with engine.begin() as connection:
             result = connection.execute(sqlalchemy.text("select version()"))
             for row in result:
@@ -55,7 +60,7 @@ def test_docker_run_mariadb(version: str):
 
 def test_docker_env_variables():
     with (
-        mock.patch.dict("os.environ", MYSQL_USER="demo", MYSQL_DATABASE="custom_db"),
+        mock.patch.dict("os.environ", MYSQL_DIALECT="pymysql", MYSQL_USER="demo", MYSQL_DATABASE="custom_db"),
         MySqlContainer("mariadb:10.6.5").with_bind_ports(3306, 32785) as container,
     ):
         url = container.get_connection_url()
@@ -75,18 +80,18 @@ def test_quoted_password():
     user = "root"
     password = "p@$%25+0&%rd :/!=?"
     quoted_password = "p%40%24%2525+0%26%25rd %3A%2F%21%3D%3F"
-    driver = "pymysql"
-    with MySqlContainer("mariadb:10.6.5", username=user, password=password) as container:
+    dialect = "pymysql"
+    with MySqlContainer("mariadb:10.6.5", dialect=dialect, username=user, password=password) as container:
         host = container.get_container_host_ip()
         port = container.get_exposed_port(3306)
-        expected_url = f"mysql+{driver}://{user}:{quoted_password}@{host}:{port}/test"
+        expected_url = f"mysql+{dialect}://{user}:{quoted_password}@{host}:{port}/test"
         url = container.get_connection_url()
         assert url == expected_url
 
         with sqlalchemy.create_engine(expected_url).begin() as connection:
             connection.execute(sqlalchemy.text("select version()"))
 
-        raw_pass_url = f"mysql+{driver}://{user}:{password}@{host}:{port}/test"
+        raw_pass_url = f"mysql+{dialect}://{user}:{password}@{host}:{port}/test"
         with pytest.raises(Exception):
             with sqlalchemy.create_engine(raw_pass_url).begin() as connection:
                 connection.execute(sqlalchemy.text("select version()"))
