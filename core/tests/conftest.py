c0653d0d72e0f5ae17ffe97a2d8abcf1f5f2d18f
Terry Smith <terry.smith@docker.com>
2025-05-07 14:05:15 -0300

    refactor: experiment with simplifying build using unreliable markers which seems to work with ibm specifically

--- core/tests/conftest.py
+++ core/tests/conftest.py
@@ -3,10 +3,7 @@ from pathlib import Path
 import pytest
 from typing import Callable
 from testcontainers.core.container import DockerClient
-from testcontainers.core.utils import is_arm
 import sys
-
-from .list_arm_extras import get_arm_extras
 
 PROJECT_DIR = Path(__file__).parent.parent.parent.resolve()
 
@@ -27,16 +24,11 @@ def python_testcontainer_image() -> str:
     py_version = ".".join(map(str, sys.version_info[:2]))
     image_name = f"testcontainers-python:{py_version}"
     client = DockerClient()
-    build_args = {"PYTHON_VERSION": py_version}
-
-    if is_arm():
-        build_args["POETRY_EXTRAS"] = get_arm_extras()
-
     client.build(
         path=str(PROJECT_DIR),
         tag=image_name,
         rm=False,
-        buildargs=build_args,
+        buildargs={"PYTHON_VERSION": py_version},
     )
     return image_name
 
