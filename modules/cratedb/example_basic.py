772a8121f34e4b0ff6f8fcbd0e06c28f3cb189fc
surister <surister98@gmail.com>
2025-10-02 12:31:55 +0200

    Add entrypoint in example_basic.py

--- modules/cratedb/example_basic.py
+++ modules/cratedb/example_basic.py
@@ -2,8 +2,15 @@ import sqlalchemy
 
 from testcontainers import cratedb
 
-with cratedb.CrateDBContainer("crate:latest", ports={4200: None, 5432: None}) as container:
-    engine = sqlalchemy.create_engine(container.get_connection_url())
-    with engine.begin() as conn:
-        result = conn.execute(sqlalchemy.text("select version()"))
-        version = result.fetchone()
+
+def main():
+    with cratedb.CrateDBContainer("crate:latest", ports={4200: None, 5432: None}) as container:
+        engine = sqlalchemy.create_engine(container.get_connection_url())
+        with engine.begin() as conn:
+            result = conn.execute(sqlalchemy.text("select version()"))
+            version = result.fetchone()
+            print(version)
+
+
+if __name__ == "__main__":
+    main()
