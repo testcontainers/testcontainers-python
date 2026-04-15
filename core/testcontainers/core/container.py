2851e483d5510c1437095e85c1dc42f4ecff3f8e
Pierre Gronlier <pierre@gronlier.fr>
2026-04-15 16:40:33 +0200

    fix(core): add container logs to TimeoutError when wait strategy times out

--- core/testcontainers/core/container.py
+++ core/testcontainers/core/container.py
@@ -217,7 +217,11 @@ class DockerContainer:
         )
 
         if self._wait_strategy is not None:
-            self._wait_strategy.wait_until_ready(self)
+            try:
+                self._wait_strategy.wait_until_ready(self)
+            except TimeoutError as ex:
+                ex.add_note(self._container.logs().decode())
+                raise ex
 
         logger.info("Container started: %s", self._container.short_id)
 
